f4d2a99493e0cfc06d264c687e6d6a2d4396ce1a
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-05 08:59:04 +0530

    try2

--- cure.py
+++ cure.py
@@ -1,3 +1,14 @@
 import numpy as np
 import plotly.graph_objs as go
 from dash import Dash, dcc, html, Input, Output
+# Sample curve data
+x = np.linspace(0, 10, 500)
+y = np.sin(x) + 1  # Shifted sine wave so area is positive
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id='curve', config={'scrollZoom': True}),
+    html.Div("Area under curve in view:", style={"marginTop": "20px"}),
+    html.Div(id='area-output')
+])
