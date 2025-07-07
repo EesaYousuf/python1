d962af8d56432e8d2e039a6d0bd1b6eab1fdba4f
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-07 11:58:12 +0530

    ops2

--- object_orinted/Bank System.py
+++ object_orinted/Bank System.py
@@ -3,3 +3,11 @@ import uuid
 # --- Custom Exception ---
 class InsufficientFundsError(Exception):
     pass
+# --- Base Class ---
+class Account:
+    bank_name = "GPT National Bank"  # class variable shared by all instances
+
+    def __init__(self, owner, balance=0):
+        self.__account_number = str(uuid.uuid4())[:8]  # private attribute
+        self.owner = owner
+        self._balance = balance  # protected attribute
