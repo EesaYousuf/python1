e437fca74e4a03401f9e5029446d82ec57acacdf
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-10 21:33:12 +0530

    try5

--- object_orinted/Multilevel Inheritance.py
+++ object_orinted/Multilevel Inheritance.py
@@ -10,5 +10,12 @@ class Father(Grandfather):
 class Son(Father):
     def laptop(self):
         print("Son: Owns a laptop.")
+ # Creating object of Son class
+s = Son()
+
+# Accessing all inherited methods
+s.house()   # Grandfather's method
+s.car()     # Father's method
+s.laptop()  # Son's method       
        
      
