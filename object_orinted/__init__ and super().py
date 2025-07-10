0aede7eb2846506a99bd209e81baca76c8d0bee7
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-10 21:35:07 +0530

    try2

--- object_orinted/__init__ and super().py
+++ object_orinted/__init__ and super().py
@@ -4,3 +4,11 @@ class Grandfather:
 
     def house(self):
         print("Grandfather: Owns a big house.")
+class Father(Grandfather):
+    def __init__(self):
+        super().__init__()  # Call Grandfather's constructor
+        print("Father's constructor called.")
+
+    def car(self):
+        print("Father: Owns a car.")
+
