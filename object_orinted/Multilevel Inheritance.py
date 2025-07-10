3ac6d82d4d871c214a71e3d73f7a4c88e38923e6
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-10 21:32:13 +0530

    try2

--- object_orinted/Multilevel Inheritance.py
+++ object_orinted/Multilevel Inheritance.py
@@ -2,3 +2,8 @@
 class Grandfather:
     def house(self):
         print("Grandfather: Owns a big house.")
+   # Derived class from Grandfather
+class Father(Grandfather):
+    def car(self):
+        print("Father: Owns a car.")
+     
