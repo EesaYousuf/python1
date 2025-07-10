f9f0a97974ae4fce6db410f21a8598d8dfafdb27
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-10 21:41:38 +0530

    try2

--- object_orinted/Hybrid Inheritance.py
+++ object_orinted/Hybrid Inheritance.py
@@ -2,3 +2,7 @@
 class Person:
     def info(self):
         print("Person: General info")
+# First level derived class
+class Father(Person):
+    def father_info(self):
+        print("Father: Engineer")        
