9a0f307db2d9a5c1d7836234fd18a3384655a5f4
eesa <eesayousufyousuf@gmail.com>
2025-07-11 20:34:19 +0530

    try2

--- oops/Composition.py
+++ oops/Composition.py
@@ -2,3 +2,10 @@
 class Engine:
     def start(self):
         return "Engine started"
+
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+
+    def start(self):
+        return self.engine.start()
