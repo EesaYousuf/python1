7a54f10af5f03f968449e673fe424a875a055df0
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-23 10:29:23 +0530

    day2

--- object_orinted/day1.py
+++ object_orinted/day1.py
@@ -6,3 +6,8 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     return x * (1 - x)
+    def generate_dataset(samples=1000):
+    """Generates a dataset for XOR operation."""
+    X = np.random.randint(0, 2, (samples, 2))
+    y = np.array([a ^ b for a, b in X])
+    return X, y.reshape(-1, 1)
